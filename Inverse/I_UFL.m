e2a4f59b3f2ab8aae34fdf402311a642df40f7ca
Discount <33273755+Dis-count@users.noreply.github.com>
2019-07-31 09:50:18 +0800

    测试atom使用matlab

--- Inverse/I_UFL.m
+++ Inverse/I_UFL.m
@@ -1,6 +1,6 @@
-  
+
 function [opt1,opt2] = I_UFL(fi,rik)   % fi rik 为列向量   求出给定 costs 时  UFL的最优值
-% 给出 Facility 数量 m  Player 数量 n 
+% 给出 Facility 数量 m  Player 数量 n
 
 % 对应限制条件数量 为 （mn+m）个  变量有（mn+m) 个
 
@@ -16,7 +16,7 @@ n = length(rik)/m ;
 
 %fprintf('Trans Costs: %f\n', rik);
 
-% fi  = [10; 10; 10; 10];   
+% fi  = [10; 10; 10; 10];
 
 % M = 100;    % define M= a bigger integer.
 
@@ -30,7 +30,7 @@ n = length(rik)/m ;
 
 
 % 产生一个 长度为 （mn+m) 的向量，每一个限制条件 产生 一个向量。
-    
+
 
 % Facility location: a company currently ships its product from 5 plants
 % to 4 warehouses. It is considering closing some plants to reduce
@@ -56,11 +56,11 @@ model.vtype = 'B';
 model.obj   = [fi; rik];
 
 % model.vtype = [repmat('B', nPlants, 1); repmat('C', nPlants * nWarehouses, 1)];
-% 
+%
 % for p = 1:nPlants
 %     model.varnames{p} = sprintf('Open%d', p);
 % end
-% 
+%
 % for w = 1:nWarehouses
 %     for p = 1:nPlants
 %         v = flowidx(w, p);
@@ -82,7 +82,7 @@ model.sense = [repmat('>', m*n , 1); repmat('=', n , 1)];
 for w =1:m
     for p =1:n
         model.A(p+n*(w-1),w) = 1;         % 第一组约束
-        
+
         model.A(p+n*(w-1),m+p+(w-1)*n) = -1;  %一定要注意这里是 m+m*n列  注意循环顺序
     end
 end
@@ -133,4 +133,3 @@ gurobi_write(model,'I_UFL.lp');
 % end
 
 end
-
