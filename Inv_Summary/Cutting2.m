36444b8ad95a43d44a0c5f3e379c5a8c88664ba6
Discount <33273755+Dis-count@users.noreply.github.com>
2020-01-13 15:14:52 +0800

    启示：写完一个函数就要测试，不然不仅会忘而且会错，一定备注

--- Inv_Summary/Cutting2.m
+++ Inv_Summary/Cutting2.m
@@ -1,28 +1,37 @@
-function x = Cutting(vi0,uij0,fi,rij)
+function x = Cutting2(vi0,uij0,fi,rij)
 % This one is used to obtain the lower bound by local search.
-
+% vi0 = [0;1;1;0;];
+% uij0 = [0; 0; 0; 0; 1; 0; 1; 0; 0; 1; 0; 1; 0; 0; 0; 0;];
+% fi = [10; 10; 10; 10;];
 % vi0, uij0 表示给定 feasible solution [0,1]  row vector
 % x 表示主问题给出的 (a,c,b,d)  2mn+2m
 % c0 = (fi;rij) 为原设施成本  column vector
 %  You must notice that the row and column vectors!!!!
+m = length(fi);
+n = length(rij)/m;
 
 s = 0; % 计数
-optcost = -0.5;
+mycost = -0.5;
 % 初始化限制集
 I = [vi0;uij0]';
 
 c0 = [fi;rij];
 
-while optcost < -0.00001
+while mycost < -0.00001
   x = Main(vi0,uij0,c0,I);
 
-  [opt1,opt2,c] = Sub(x,fi,rij,vi0,uij0);
+  c = (x(1:m*n+m)-x(m*n+m+1:2*m*n+2*m)+[fi;rij]);
 
-  opt1 = round(opt1);  % Rounding the fraction to integer.
+  opt2 = dot(c,[vi0;uij0]);
 
-  [optcost,optsol] = LocalS(opt1,opt2,c,fi,rij); % Local search give the neighborhood solution
+  opt1 = [vi0;uij0];
 
-  I = [I;optsol'];
+  [mycost,opt_sol] = LocalS(opt1,opt2,c,m,n); % Local search give the neighborhood solution
+
+  vi0 = opt_sol(1:m);
+  uij0 = opt_sol(m+1:end);
+
+  I = [I;opt_sol'];
 
   s = s + 1;
 
@@ -91,70 +100,7 @@ opt = res.x;   %  给出 (a,c,b,d)
 end
 
 
-function [opt1,opt2,c] = Sub(x,fi,rij,vi0,uij0)
-% I expresses restrict set. Every row is a feasible solution
-% And the row numbers are the number of solutions
-% x 表示主问题给出的 (a,c,b,d)  2mn+2m
-% c0 = (fi;rij) 为原设施成本  Column vector
-
-m = length(fi) ;
-
-n = length(rij)/m ;
-
-c = (x(1:m*n+m)-x(m*n+m+1:2*m*n+2*m)+[fi;rij]);  % m*n+m
-
-model.modelname = 'Sub';
-
-model.modelsense = 'min';
-
-ncol = m*n + m ;
-
-model.vtype = 'C';
-
-model.obj   = c ;
-
-nrow = m*n + n;
-
-model.A     = sparse(nrow, ncol);
-
-model.rhs   = [zeros(m*n, 1); ones(n,1)];
-
-model.sense = [repmat('>', m*n , 1); repmat('=', n , 1)];
-
-for w =1:m
-    for p =1:n
-        model.A(p+n*(w-1),w) = 1;
-
-        model.A(p+n*(w-1),m+p+(w-1)*n) = -1;
-
-    end
-end
-
-for p = 1:n
-
-    model.A(p+m*n, n*(0:(m-1))+m+p) = 1;
-
-end
-% Save model
-% gurobi_write(model,'Sub.lp');
-
-% Set parameters
-% params.method = 2;
-
-% Optimize
-% res = gurobi(model, params);
-
-params.outputflag = 0;
-
-res = gurobi(model,params);
-
-opt1 = res.x;   %  给出 (v*,u*)
-
-opt2 = dot(c,[vi0;uij0]);
-
-end
-
-function [optcost,opt_sol] = LocalS(opt1,opt2,c,m,n)
+function [mycost,opt_sol] = LocalS(opt1,opt2,c,m,n)
 % opt1 is a suboptimal solution
 % c0 = (fi;rij) 为原设施成本  Column vector
 % opt is a local optimal solution
@@ -164,7 +110,7 @@ fi = c(1:m);
 
 rij = c(m+1:end);
 
-opt0 = dot(c,opt1) - opt2;
+opt0 = dot(c,opt1) ;  % Initial feasible cost
 
 % Define the number of opening and not opening facilities
 facility = opt1(1:m);  % Get the first m vectors.
@@ -178,25 +124,27 @@ ind_unopen = setdiff(1:m,ind_open)'; % Obtain the Set Difference
 
 optcost = opt0;
 
+opt_sol = opt1;
+
 for i = 1:2*(m+n)  % Control the number of iteration.
 
   if unopen > 0
     % Define the add-move   p kinds of local search
-    [opt_sol1,cost1] = add0(fi,trans,ind_open,ind_unopen)
+    [opt_sol1,cost1] = add0(fi,trans,ind_open,ind_unopen);
   end
   % Define the swap-move   p(m-p) kinds of local search
   if unopen > 0
 
-    [opt_sol2,cost2] = swap(fi,trans,ind_open,ind_unopen)
+    [opt_sol2,cost2] = swap(fi,trans,ind_open,ind_unopen);
   end
 
 % Define the remove   m-p  kinds of local search
   if open1 > 1
 
-    [opt_sol3,cost3] = remove0(fi,trans,ind_open,ind_unopen)
+    [opt_sol3,cost3] = remove0(fi,trans,ind_open);
   end
 
-  [opt_cost,opt_ind] = min(cost1,cost2,cost3)
+  [opt_cost,opt_ind] = min([cost1,cost2,cost3]);
 
   optsol = [opt_sol1,opt_sol2,opt_sol3];
 
@@ -204,7 +152,7 @@ for i = 1:2*(m+n)  % Control the number of iteration.
 
     optcost = opt_cost; % obtain the suboptimal cost
 
-    opt_sol = optsol[:,opt_ind];
+    opt_sol = optsol(:,opt_ind);
 
     opt_fac = opt_sol(1:m);
 
@@ -219,5 +167,7 @@ for i = 1:2*(m+n)  % Control the number of iteration.
   end
 
 end
+
+mycost = optcost - opt2;
 
 end
--- Inv_Summary/Local.m
+++ Inv_Summary/Local.m
@@ -1,4 +1,4 @@
-function [optcost,opt_sol] = LocalS(opt1,opt2,c,m,n)
+function [mycost,opt_sol] = Local(opt1,opt2,c,m,n)
 % opt1 is a suboptimal solution
 % c0 = (fi;rij) 为原设施成本  Column vector
 % opt is a local optimal solution
@@ -8,7 +8,7 @@ fi = c(1:m);
 
 rij = c(m+1:end);
 
-opt0 = dot(c,opt1) - opt2;
+opt0 = dot(c,opt1) ;  % Initial feasible cost
 
 % Define the number of opening and not opening facilities
 facility = opt1(1:m);  % Get the first m vectors.
@@ -26,18 +26,18 @@ for i = 1:2*(m+n)  % Control the number of iteration.
 
   if unopen > 0
     % Define the add-move   p kinds of local search
-    [opt_sol1,cost1] = add0(fi,trans,ind_open,ind_unopen,m,n);
+    [opt_sol1,cost1] = add0(fi,trans,ind_open,ind_unopen);
   end
   % Define the swap-move   p(m-p) kinds of local search
   if unopen > 0
 
-    [opt_sol2,cost2] = swap(fi,trans,ind_open,ind_unopen,m,n);
+    [opt_sol2,cost2] = swap(fi,trans,ind_open,ind_unopen);
   end
 
 % Define the remove   m-p  kinds of local search
   if open1 > 1
 
-    [opt_sol3,cost3] = remove0(fi,trans,ind_open,ind_unopen,m,n);
+    [opt_sol3,cost3] = remove0(fi,trans,ind_open);
   end
 
   [opt_cost,opt_ind] = min([cost1,cost2,cost3]);
@@ -64,30 +64,33 @@ for i = 1:2*(m+n)  % Control the number of iteration.
 
 end
 
+mycost = optcost - opt2;
+
 end
 
 
 % opt1=[
-%     0
-%     1
-%     1
-%     0
-%     0
-%     0
-%     0
-%     0
-%     1
-%     0
-%     1
-%     0
-%     1
-%     1
-%     0
-%     0
-%     0
-%     0
-%     0
-%     0];
+%       0
+%       1
+%       1
+%       0
+%       0
+%       0
+%       0
+%       0
+%       1
+%       0
+%       1
+%       0
+%       1
+%       1
+%       0
+%       0
+%       0
+%       0
+%       0
+%       0];
+
 % opt2 =0;
 %
 % M =20;
